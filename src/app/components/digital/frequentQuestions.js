26ca010a383dbecbde79aabbead4e25de5ed739a
dcdte <federico.lopezvi@gmail.com>
2025-04-15 10:21:16 -0500

    continue project

--- src/app/components/digital/frequentQuestions.js
+++ src/app/components/digital/frequentQuestions.js
@@ -1,32 +1,59 @@
 import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/autoplay";
+
 import logoConinsa from "/public/digital/logo-coninsa.png";
 import logoViva from "/public/digital/logo-viva.png";
 import logoNutresa from "/public/digital/logo-nutresa.png";
 import logoLondonio from "/public/digital/logo-londonio.png";
 import logoIdeo from "/public/digital/logo-ideo.png";
 import logoArquitectura from "/public/digital/logo-arquitectura.png";
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Typography,
-} from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export default function FrequentQuestions() {
+  const companies = [
+    logoConinsa,
+    logoViva,
+    logoNutresa,
+    logoLondonio,
+    logoIdeo,
+    logoArquitectura,
+  ];
+
   return (
     <div>
       <div>
-        <h3>
+        <h3 className="text-[#666AAC] text-[24px] mb-5 mt-14 lg:mt-30 2xl:mt-40 text-center">
           CONFÍAN <span>EN NOSOTROS</span>
         </h3>
-        <div>
-          <Image src={logoConinsa} alt="iconDigital1" />
-          <Image src={logoViva} alt="iconDigital1" />
-          <Image src={logoNutresa} alt="iconDigital1" />
-          <Image src={logoLondonio} alt="iconDigital1" />
-          <Image src={logoIdeo} alt="iconDigital1" />
-          <Image src={logoArquitectura} alt="iconDigital1" />
+        <div className="bg-[rgba(255,255,255,0.05)] w-full flex justify-center items-center gap-4 py-4 border border-[#2BFFC3]/10 rounded-[7px]">
+          <Swiper
+            modules={[Autoplay]}
+            slidesPerView="auto"
+            spaceBetween={20}
+            loop={true}
+            speed={3000}
+            autoplay={{
+              delay: 0,
+              disableOnInteraction: false,
+            }}
+            className="flex items-center justify-center"
+          >
+            {companies.map((company, index) => (
+              <SwiperSlide key={index} style={{ width: "auto" }}>
+                <div className="flex items-center justify-center h-10">
+                  <Image
+                    src={company}
+                    alt={`logo-${index}`}
+                    className="max-h-10 w-auto object-contain"
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
       <div>
